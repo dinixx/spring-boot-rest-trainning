aa3a46ed09f2d09a5ba9b0254efac669d8d84944
Diego Henriques Barboza Vieira <diegohenrix@gmail.com>
2019-09-28 16:35:21 -0300

    setting up project

---
+++ src/main/java/com/dnx/restconcepts/RestConceptsApplication.java
@@ -0,0 +1,13 @@
+package com.dnx.restconcepts;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestConceptsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestConceptsApplication.class, args);
+	}
+
+}
