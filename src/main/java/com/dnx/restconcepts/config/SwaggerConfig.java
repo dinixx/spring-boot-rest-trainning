e3c93077e9e3193191fdde297982a02c0b4359b4
Diego Henriques Barboza Vieira <diegohenrix@gmail.com>
2019-09-30 22:33:27 -0300

    customizing Swagger

--- src/main/java/com/dnx/restconcepts/config/SwaggerConfig.java
+++ src/main/java/com/dnx/restconcepts/config/SwaggerConfig.java
@@ -3,8 +3,11 @@ package com.dnx.restconcepts.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
@@ -18,7 +21,20 @@ public class SwaggerConfig {
 
   @Bean
   public Docket api() {
-    return new Docket(DocumentationType.SWAGGER_2).select().apis(RequestHandlerSelectors.any())
-        .paths(PathSelectors.any()).build();
+    return new Docket(DocumentationType.SWAGGER_2).select()
+        .apis(RequestHandlerSelectors.basePackage("com.dnx.restconcepts.controller")).paths(PathSelectors.any())
+        .build().apiInfo(apinfo());
   }
+
+  private ApiInfo apinfo() {
+    return new ApiInfoBuilder()
+            .title("Demo Documentation")
+            .description("Rest API that provides programming quotes")
+            .termsOfServiceUrl("Some Terms of Services URL")
+            .version("1.0.0")
+            .license("Some License Info")
+            .licenseUrl("Some License URL")
+            .contact(new Contact("Diego Vieira", "https://github.com/dinixx", "diegohenrix@gmail.com"))
+            .build();
+}
 }
--- src/main/java/com/dnx/restconcepts/controller/QuoteController.java
+++ src/main/java/com/dnx/restconcepts/controller/QuoteController.java
@@ -5,22 +5,40 @@ import com.dnx.restconcepts.service.QuoteService;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiParam;
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
 
 /**
  * QuoteController
  */
 @RestController
 @RequestMapping("/quotes")
+@Api(tags = "Quotes", description = "Demo Quote Rest API")
 public class QuoteController {
 
   @Autowired
   QuoteService quoteService;
 
   @GetMapping("/random")
+  @ApiOperation(produces = "application/json", protocols = "http", value = "ramdon")
   public Quote random() {
     return quoteService.random();
+  }
+
+  @GetMapping("/{id}")
+  @ApiOperation(produces = "application/json", protocols = "http", value = "getQuote")
+  @ApiResponses(value = { @ApiResponse(code = 200, message = "Success!") })
+  public Quote getQuote(@ApiParam("Id of quote, can not be null") @PathVariable int id) {
+    Quote quote = new Quote();
+    quote.setAuthor("to be done");
+    return quote;
   }
 
 }
